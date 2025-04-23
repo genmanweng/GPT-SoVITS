8ddc23cd71004668b01c6c5a99b9e0916255f37a
Genman <genman@twim.cc>
2025-04-23 10:23:57 +0800

    add

--- run_gpt.sh
+++ run_gpt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9901 > log_9901.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9902 > log_9902.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9903 > log_9903.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9904 > log_9904.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9905 > log_9905.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9906 > log_9906.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9907 > log_9907.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9908 > log_9908.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9909 > log_9909.out 2>&1 &
-nohup python GPT_SoVITS/inference_webui_fast.py --port 9910 > log_9910.out 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9901 > /home/log/log_9901.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9902 > /home/log/log_9902.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9903 > /home/log/log_9903.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9904 > /home/log/log_9904.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9905 > /home/log/log_9905.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9906 > /home/log/log_9906.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9907 > /home/log/log_9907.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9908 > /home/log/log_9908.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9909 > /home/log/log_9909.log 2>&1 &
+nohup python GPT_SoVITS/inference_webui_fast.py --port 9910 > /home/log/log_9910.log 2>&1 &
